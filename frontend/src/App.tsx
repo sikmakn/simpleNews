6e4304cafcdc00b0d8c86a6166dfb92153e9aff2
Никита Колчанов <sikmakn@gmail.ru>
2020-10-30 00:55:43 +0300

    add router/ create main page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import './wdyr';
 
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+import MainPage from './pages/mainPage';
 
 function App() {
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={MainPage}/>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
