614e7fbdb5c0dcc23ad0d4964d5ddfdf307f92b6
Никита Колчанов <sikmakn@gmail.ru>
2020-11-08 03:48:09 +0300

    add signInForm

--- frontend/src/components/sigInForm/index.tsx
+++ frontend/src/components/sigInForm/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import styles from './signIn.module.scss';
+
+import UserFormButton from '../userFormButton';
 
 const SignInForm: React.FC = () => {
+    const loginInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
     return (
-        <>
+        <div className={styles.form}>
+            <input
+                className={styles.login}
+                ref={loginInputRef}
+                type="text" placeholder="Логин"
+            />
+            <input
+                className={styles.password}
+                ref={passwordInputRef}
+                type="password" placeholder="Пароль"
+            />
+            <UserFormButton title={'Войти'} onClick={() => {
+                if (!loginInputRef?.current || !passwordInputRef?.current) return;
 
-        </>
+                const login = loginInputRef.current.value;
+                const password = passwordInputRef.current.value;
+
+            }}/>
+        </div>
     );
 };
 
